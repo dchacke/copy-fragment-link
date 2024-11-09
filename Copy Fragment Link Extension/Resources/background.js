0a69a67130b10df0fada6acc710d8d1a04a5a22b
Dennis Hackethal <dh@dennishackethal.com>
2024-11-09 00:30:47 -0700

    Add option to copy fragment path rather than entire URL

--- Copy Fragment Link Extension/Resources/background.js
+++ Copy Fragment Link Extension/Resources/background.js
@@ -1,29 +1,44 @@
 // Create a context menu item when the extension is installed
 browser.contextMenus.create({
-  id: 'customCopyFragmentLink',
-  title: 'Copy fragment link',
-  contexts: ['selection'], // Only show when there's selected text
+  id: 'copyFragmentUrl',
+  title: 'Copy fragment URL',
+  contexts: ['selection'] // Only show when there's selected text
+});
+
+browser.contextMenus.create({
+  id: 'copyFragmentPath',
+  title: 'Copy fragment path',
+  contexts: ['selection']
 });
 
 // Handle the click event for the context menu
 browser.contextMenus.onClicked.addListener((info, tab) => {
-  if (info.menuItemId === 'customCopyFragmentLink') {
-    const selectedText = info.selectionText;
-
-    if (selectedText) {
-      // Encode the selected text to be safely used in a URL fragment
-      const encodedText = encodeURIComponent(selectedText);
-
-      // Construct the fragment link (using the current page's URL)
-      const fragmentLink = `${tab.url}#:~:text=${encodedText}`;
-
-      // Copy the link to the clipboard
-      copyToClipboard(fragmentLink);
-    } else {
-      console.log('No text selected');
-    }
+  if (info.menuItemId != 'copyFragmentUrl' && info.menuItemId != 'copyFragmentPath') {
+    return;
   }
+
+  const selectedText = info.selectionText;
+
+  if (!selectedText) {
+    return;
+  }
+
+  const encodedText = encodeURIComponent(selectedText);
+
+  copyToClipboard(urlOrPath(info, tab, encodedText));
 });
+
+function urlOrPath(info, tab, encodedText) {
+  let fragment = `#:~:text=${encodedText}`;
+
+  if (info.menuItemId === 'copyFragmentUrl') {
+    return tab.url.split('#')[0] + fragment;
+  } else if (info.menuItemId === 'copyFragmentPath') {
+    const path = (new URL(tab.url)).pathname;
+
+    return path + fragment;
+  }
+}
 
 function copyToClipboard(text) {
   navigator.clipboard.writeText(text)
